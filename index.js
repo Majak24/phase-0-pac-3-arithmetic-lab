31d50240479d40014032273485a37cc207400143
Majak Deng <majakbanks.deng>
2025-06-03 13:34:39 +0300

    modified

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(x,y) {
+    return x + y 
+}
+add(2,3);
+
+function subtract(a,b) {
+    return a - b
+}
+subtract(2,3);
+
+function multiply(a,b) {
+    return a * b
+}
+multiply(2,3);
+
+function divide(a,b) {
+    return a/b;
+}
+divide(3,5);
+function increment(n) {
+    return n + 1;
+}
+function decrement(n) {
+    return n - 1;
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}   
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
